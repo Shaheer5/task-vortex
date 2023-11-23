13b68bd3b900b1a9d8fe0927d99705bb46942d98
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-11-23 17:05:54 +0500

    boiler plate for create.jsx

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 // styles
 import './Create.css';
 
-
 export default function Create() {
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [category, setCategory] = useState('');
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    console.log(name, details, dueDate)
+  }
+
   return (
     <div>Create</div>
   )
