1e9e4ae5c0b27231e12a83a3af0d93ce4254b99c
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-11-29 19:47:39 +0500

    update: displaying error when !document

--- src/hooks/useDocument.jsx
+++ src/hooks/useDocument.jsx
@@ -2,14 +2,18 @@ import { useState, useEffect } from 'react';
 import { projectFirestore } from '../firebase/config';
 
 export const useDocument = (collection, id) => {
-  const [ document, setDocument ] = useState(null);
-  const [ error, setError ] = useState(null);
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const ref = projectFirestore.collection(collection).doc(id);
     const unsub = ref.onSnapshot(snapshot => {
-      setDocument({ ...snapshot.data(), id: snapshot.id })
-      setError(null);
+      if (snapshot.data()) {
+        setDocument({ ...snapshot.data(), id: snapshot.id })
+        setError(null);
+      } else {
+        setError("Couldn't fetch the Document")
+      }
     }, (err) => {
       console.log(err.message);
       setError('failed to get document')
@@ -18,5 +22,5 @@ export const useDocument = (collection, id) => {
     return () => unsub();
   }, [collection, id])
 
-  return {document, error};
+  return { document, error };
 }
